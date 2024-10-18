f013135ae18f017cecb8a285ce6dd29c740c1cbf
Fractalo <42487844+fractalo@users.noreply.github.com>
2024-10-18 15:16:33 +0900

    chore: initial commit

---
+++ src/main/java/com/github/fractalo/streaming_settlement/StreamingSettlementApplication.java
@@ -0,0 +1,13 @@
+package com.github.fractalo.streaming_settlement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StreamingSettlementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StreamingSettlementApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/fractalo/streaming_settlement/StreamingSettlementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.fractalo.streaming_settlement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StreamingSettlementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
